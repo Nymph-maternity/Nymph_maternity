fa57485e0dea9de46da6467febbf46f63226a296
halimatcd <aminuoluwatosin2000@gmail.com>
2021-03-03 22:50:00 +0100

    updated link title

--- view/login.html.php
+++ view/login.html.php
@@ -13,7 +13,7 @@
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
     <div class="container-fluid">
-      <a class="navbar-brand text-white" href="#"> THE MATERNITY SOLUTION</a>
+      <a class="navbar-brand text-white" href="#"> THE MATERNITY SOLUTION and i changed it</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
